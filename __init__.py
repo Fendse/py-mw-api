ca5b7d6534316c19d6d48c4930d91a8a95ce64a4
Sara Johnsson <46252070+Fendse@users.noreply.github.com>
2021-05-23 21:33:46 +0200

    Add __init__

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .mw_api import MWApiParser, MediaWikiWarning, MediaWikiError, MWApiSession
+
+__all__ = [
+    "MWApiParser", "MediaWikiWarning", "MediaWikiError", "MWApiSession",
+    "encapsulating_argparse"
+]
