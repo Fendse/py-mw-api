5b21fb368c106db4528f1e74fe78f6e9dc3e9e08
Sara Johnsson <46252070+Fendse@users.noreply.github.com>
2021-05-24 02:18:18 +0200

    Allow login by password file

--- parser.py
+++ parser.py
@@ -17,31 +17,37 @@ class MWApiParser(EncapsulatingParser):
         super(MWApiParser, self).__init__(after_full_parse=after_full_parse, **kwargs)
 
         if api_root is None:
-            self.add_argument("API_root", type=str, metavar="API-ROOT")
+            self.add_argument("API_root", type=str, metavar="API_ROOT")
         else:
-            self.add_argument("--api-root", type=str, metavar="API-ROOT", default=api_root)
+            self.add_argument("--api-root", type=str, metavar="API_ROOT", default=api_root, dest="API_root")
 
         self.add_argument("--username", "--user", type=str, metavar="USERNAME", default=argparse.SUPPRESS)
         auth_mutex = self.add_mutually_exclusive_group()
         auth_mutex.add_argument("--password", type=str, metavar="PASSWORD", default=argparse.SUPPRESS)
-        # TODO: Allow reading password from a file
+        auth_mutex.add_argument("--password-file", metavar="PASSWORD_FILE", type=str, default=argparse.SUPPRESS)
 
         self.add_argument("--assert", type=str, choices=["user", "bot"])
 
     def _login(parser, namespace):
-        has_password = hasattr(namespace, "password")
+        has_password = hasattr(namespace, "password") or hasattr(namespace, "password_file")
         has_username = hasattr(namespace, "username")
 
         namespace.session = MWApiSession(namespace.API_root)
 
         if has_username and has_password:
-            namespace.session.login(namespace.username, namespace.password)
+            if (hasattr(namespace, "password")):
+                password = namespace.password
+                del(namespace.password)
+            else:
+                with open(namespace.password_file, "rt") as password_file:
+                    password = password_file.read()
+                del(namespace.password_file)
+            namespace.session.login(namespace.username, password)
             del(namespace.username)
-            del(namespace.password)
         elif has_username:
             del(namespace.username)
-            parser.error("Missing argument: PASSWORD")
+            parser.error("Must not specify --username without either --password or --password-file")
         elif has_password:
             del(namespace.password)
-            parser.error("Missing argument: USERNAME")
+            parser.error("Must not specify --password or --password-file without --username")
 
